02f9661d2451a1e04142104eb3615242daf33226
Kristina Mnatsakanian <kristina.m1999@yandex.ru>
2023-06-19 21:26:46 +0600

    feat: implement streams functions

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import {createWriteStream} from "fs";
+import {dirname, join} from "path";
+import {fileURLToPath} from "url";
+const { stdin} = process;
+
 const write = async () => {
-    // Write your code here 
+    const pathToFile = join(dirname(fileURLToPath(import.meta.url)), 'files', 'fileToWrite.txt');
+    try {
+        const writeStream = createWriteStream(pathToFile, 'utf-8');
+        stdin.on('data', chunk => writeStream.write(chunk));
+    } catch (e) {
+        console.log(e.message);
+    }
 };
 
 await write();
