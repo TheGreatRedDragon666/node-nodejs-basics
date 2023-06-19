a5376e57421e3ffe5a85013678d980283c3cb70e
Kristina Mnatsakanian <kristina.m1999@yandex.ru>
2023-06-19 19:42:40 +0600

    feat: complete functions in fs

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,14 @@
+import fs from 'fs/promises';
+import {dirname, join} from 'path';
+import {fileURLToPath} from "url";
+
 const rename = async () => {
-    // Write your code here 
+    const pathToDir = join(dirname(fileURLToPath(import.meta.url)), 'files');
+    try {
+        await fs.rename(join(pathToDir, 'wrongFilename.txt'), join(pathToDir, 'properFilename.md'));
+    } catch (e) {
+        throw new Error("FS operation failed: " + e.message);
+    }
 };
 
 await rename();
