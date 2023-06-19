83f247f3d1491d9343c529998036d03d044ee213
Kristina Mnatsakanian <kristina.m1999@yandex.ru>
2023-06-19 20:24:24 +0600

    feat: complete functions in cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    const result = [];
+   Object.entries(process.env).filter(([key, value]) => key.startsWith('RSS_')).forEach(([key, value]) => {
+       result.push(`${key}=${value}`);
+    })
+    console.log(result.join('; '));
 };
 
 parseEnv();
