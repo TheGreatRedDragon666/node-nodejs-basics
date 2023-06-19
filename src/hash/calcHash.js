b538bf768372090fa011b6195cd2f75ef48d0ceb
Kristina Mnatsakanian <kristina.m1999@yandex.ru>
2023-06-19 20:58:30 +0600

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import fs from 'fs/promises';
+import {dirname, join} from "path";
+import {fileURLToPath} from "url";
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const pathToFile = join(dirname(fileURLToPath(import.meta.url)), 'files', 'fileToCalculateHashFor.txt');
+    try {
+        const data = await fs.readFile(pathToFile);
+        const hash = crypto.createHash('sha256').update(data).digest('hex');
+        console.log(hash);
+    } catch (e) {
+        console.log(e.message);
+    }
 };
 
 await calculateHash();
